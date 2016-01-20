e7feb9715099293d8fb2a6bfbd14fe4c7038f4ea
Akilgour <kilgourkilgour@hotmail.com>
2016-01-20 11:04:29 +0000

    added SystemTimeTests and they all fail, als added nunint to project

---
+++ Helper/SystemTime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public class SystemTime
+    {
+        private static DateTime datetime;
+
+        public static void Reset()
+        {
+            datetime = DateTime.MinValue;
+        }
+
+        public static DateTime Now
+        {
+            get
+            {
+                return new DateTime(1983, 8, 27);
+            }
+
+        }
+
+        public static void Set(DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HelperTest/SystemDateTimeTest.cs
@@ -0,0 +1,77 @@
+﻿using Helper;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperTest
+{
+    [TestFixture]
+    public class SystemDateTimeTest
+    {
+        [Test]
+        public void SystemTimeIsNow()
+        {
+
+            //Arrange
+            var now = DateTime.Now;
+
+            //Act
+            var systemNow = SystemTime.Now;
+
+            //Asset 
+            Assert.AreEqual(now, systemNow);
+        }
+
+        [Test]
+        public void SystemTimeShortDateIsNow()
+        {
+
+            //Arrange
+            var now = DateTime.Now.ToShortDateString();
+
+            //Act
+            var systemNow = SystemTime.Now.ToShortDateString();
+
+            //Asset 
+            Assert.AreEqual(now, systemNow);
+        }
+
+        [Test]
+        public void SystemTimeReset()
+        {
+
+            //Arrange
+            var now = DateTime.Now;
+            var notNow = new DateTime(2001, 1, 3, 3, 4, 5);
+
+            //Act
+            SystemTime.Set(notNow);
+            SystemTime.Reset();
+            var systemNow = SystemTime.Now;
+
+            //Asset 
+            Assert.AreEqual(now, systemNow);
+        }
+
+        [Test]
+        public void SystemTimeSet()
+        {
+
+            //Arrange
+            var now = DateTime.Now;
+            var notNow = new DateTime(2001, 1, 3, 3, 4, 5);
+
+            //Act
+            SystemTime.Set(notNow);
+            var systemNow = SystemTime.Now;
+
+            //Asset 
+            Assert.AreEqual(now, systemNow);
+        }
+
+
+    }
+}
